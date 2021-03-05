a9feab0b2d94056a2c2dfc1f13463d2c859d0cac
liaoyf <liaoy@sunsharing.com.cn>
2021-03-05 10:42:31 +0800

    fix: 修复 publicPath 问题

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './static/main.js',
     mode: "production",
     output: {
-        publicPath: ""
+        publicPath: "/my-blog"
     },
     module: {
         rules: [
